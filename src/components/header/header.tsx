f71ce488825d003b870fe5d1aa8af4e2871b34d7
pedro-novo <pedro.novo.93@gmail.com>
2023-10-22 15:49:37 +0100

    remove margin

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
   return (
     <nav className="dark:bg-gray-800">
       <div className="mx-auto max-w-7xl xl:px-6 2xl:px-8">
-        <div className="py-6 px-6 lg:px-12 w-full h-full mb-8 flex items-center justify-between">
+        <div className="py-6 px-6 lg:px-12 w-full h-full flex items-center justify-between">
           <h1 className="text-2xl font-bold uppercase dark:text-white">Pedro Novo</h1>
           <div className="flex items-center justify-center gap-4">
             <DarkModeToggle />
