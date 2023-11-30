aa81d30623f25cb03c7fd8443646b65f79c3f4c8
pedro-novo <pedro.novo.93@gmail.com>
2023-11-30 06:53:51 +0000

    update pedro feedback

--- src/components/referrals/referrals.tsx
+++ src/components/referrals/referrals.tsx
@@ -71,9 +71,12 @@ const people = [
     company: "ING",
     imageUrl:
       "https://media.licdn.com/dms/image/C4D03AQFPBWXO-AvQpw/profile-displayphoto-shrink_400_400/0/1617751299753?e=1703721600&v=beta&t=wEu1WG-HYmJwtNkhOehH1u_mYXKUudsfOeBPBHxsq_0",
-    feedback: `It's been a pleasure working with you on our full-stack project. I should mention that initially, I was aware of your relatively short experience as a developer, but I was surprised by your strong understanding of coding practices, your proficiency with React and TypeScript, and your knowledge of component libraries such as MUI and Tailwind. 
-    What stood out the most was the fact that as soon as you joined the project you identified anomalies that would make the project difficult to scale and maintain in the future, and from this came your initiative to rebuild the UI from scratch, addressing these issues and also adding a personal touch to create user interfaces that would take it to the next level.
-    I must thank you for the suggestion you gave me when I read the book "The Tao of React", as you warned me of some bad practices and coding habits I had that were affecting the quality of the code I was delivering. Your guidance has been fundamental to my growth as a developer and the growth of the project, and for that I thank you very much.`,
+    feedback: `Pedro is a forward-thinking developer with a wealth of knowledge.
+    When I had the opportunity to work with Pedro, I was aware that his background as a developer was relatively short, but once we started working together, I realized how irrelevant years of experience are.
+        
+    Once you have Pedro as a resource, you won't be able to keep him out of your ideas or thoughts, whether they are related to UI/UX, API development or organizing a data model. His pragmatic approach always manages to raise questions that encourage you to think about what needs to be redefined or improved.
+        
+    Pedro has qualities that will ensure his success in the long run: he consistently produces work of a high standard and he is driven to learn everything he can.`,
     twitterUrl: "#",
     linkedinUrl: "https://www.linkedin.com/in/pedro-areal-torres/",
     companyUrl: "https://www.ing.com/Home.htm",
@@ -105,7 +108,9 @@ const Referrals: React.FC = () => {
                   src={person.imageUrl}
                   alt={person.name}
                 />
-                <h3 className="mt-4 text-lg font-semibold leading-8 text-gray-900">{person.name}</h3>
+                <h3 className="mt-4 text-lg font-semibold leading-8 text-gray-900">
+                  {person.name}
+                </h3>
                 <p className="text-sm leading-7 text-gray-600">
                   {person.role}{" "}
                   <span className="text-gray-600 font-bold hover:text-gray-400">
@@ -114,12 +119,19 @@ const Referrals: React.FC = () => {
                     </a>
                   </span>
                 </p>
-                <p className="mt-4 text-base leading-7 text-gray-600 whitespace-pre-line">{person.feedback}</p>
+                <p className="mt-4 text-base leading-7 text-gray-600 whitespace-pre-line">
+                  {person.feedback}
+                </p>
                 <ul role="list" className="mt-6 flex gap-x-6">
                   <li>
                     <a href={person.twitterUrl} className="text-gray-400 hover:text-gray-500">
                       <span className="sr-only">Twitter</span>
-                      <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                      <svg
+                        className="h-5 w-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
                         <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
                       </svg>
                     </a>
@@ -127,7 +139,12 @@ const Referrals: React.FC = () => {
                   <li>
                     <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
                       <span className="sr-only">LinkedIn</span>
-                      <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                      <svg
+                        className="h-5 w-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
                         <path
                           fillRule="evenodd"
                           d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
